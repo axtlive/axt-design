39f61ac61badc038870ed9bdcca01b623837960a
axtlive <axtlive@foxmail.com>
2020-12-27 19:13:22 +0800

    测试使用axios进行upload

---
+++ src/1App.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import Button from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
+import SubMenu from "./components/Menu/subMenu";
+import Icon from "./components/Icon/icon";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Menu
+          onSelect={(i) => console.log(i)}
+          defaultIndex={"0"}
+          defaultOpenSubMenus={["2"]}
+        >
+          <MenuItem>菜单a菜单a</MenuItem>
+          <MenuItem disabled>菜单b菜单b</MenuItem>
+          <SubMenu title="菜单c菜单c">
+            <MenuItem>dropdown1</MenuItem>
+            <MenuItem>dropdown2</MenuItem>
+            <MenuItem>dropdown3</MenuItem>
+          </SubMenu>
+          <MenuItem>菜单d菜单d</MenuItem>
+          <MenuItem>菜单e菜单e</MenuItem>
+        </Menu>
+      </div>
+
+      <div>
+        <Button onClick={() => console.log("---------")}>default Button</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button btnType="primary" size="lg">
+          primary large Button
+        </Button>
+        <Button btnType="danger" size="sm">
+          Danger Small Button
+        </Button>
+        <Button btnType="link" href="http://www.baidu.com" target="_blank">
+          Link Button
+        </Button>
+        <Button btnType="link" href="http://www.baidu.com" disabled>
+          Link disabled Button
+        </Button>
+      </div>
+
+      <div>
+        <Icon icon="search" theme="primary" size="10x" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
